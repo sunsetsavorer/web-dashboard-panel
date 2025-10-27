d52fcaab7d54f6d2c054f6e0359bb517cddb5f83
sunsetsavorer <sunset.savorer@gmail.com>
2025-10-28 00:22:02 +0300

    feat: cmd app (entrypoint)

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/sunsetsavorer/web-dashboard-panel/internal/app"
+)
+
 func main() {
 
+	app := app.New()
+
+	err := app.Run()
+	if err != nil {
+		fmt.Println(err)
+	}
 }
